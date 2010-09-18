c537471f71e5fbc7a64c8ef3bf0dfb18905e7f24
Germán Póo-Caamaño <gpoo@gnome.org>
2010-09-18 11:09:53 -0700

    Use a dict of patterns instead of several global variables
    The dictionary used allows the use of a single meaningful
    variable with a code cleaner and easier to read.  Also,
    it is not harder to add new patterns.
    
    Signed-off-by: Germán Póo-Caamaño <gpoo@gnome.org>

--- patterns.py
+++ patterns.py
@@ -1,10 +1,12 @@
 #
+# -*- coding:utf-8 -*-
 # Pull together regular expressions used in multiple places.
 #
 # This code is part of the LWN git data miner.
 #
 # Copyright 2007-8 LWN.net
 # Copyright 2007-8 Jonathan Corbet <corbet@lwn.net>
+# Copyright 2009 Germán Póo-Caamaño <gpoo@gnome.org>
 #
 # This file may be distributed under the terms of the GNU General
 # Public License, version 2.
@@ -16,26 +18,30 @@ import re
 # expressions." Now they have two problems.
 #    -- Jamie Zawinski
 #
-Pemail = r'\s+"?([^<"]+)"?\s<([^>]+)>' # just email addr + name
-Pcommit = re.compile (r'^commit ([0-9a-f ]+)$')
-Pauthor = re.compile (r'^Author:' + Pemail + '$')
-Psob = re.compile (r'Signed-off-by:' + Pemail)
-Pmerge = re.compile (r'^Merge:.*$')
-Pdate = re.compile (r'^(Commit)?Date:\s+(.*)$')
-Pfilea = re.compile (r'^---\s+(.*)$')
-Pfileb = re.compile (r'^\+\+\+\s+(.*)$')
-Preview = re.compile (r'Reviewed-by:' + Pemail)
-Ptest = re.compile (r' tested-by:' + Pemail, re.I)
-Prep = re.compile (r'Reported-by:' + Pemail)
-Preptest = re.compile (r'reported-and-tested-by:' + Pemail, re.I)
-#
-# Merges are described with a variety of lines.
-#
-PExtMerge = re.compile(r'^ +Merge( branch .* of)? ([^ ]+:[^ ]+)\n$')
-PIntMerge = re.compile(r'^ +(Merge|Pull) .* into .*$')
-# PIntMerge2 = re.compile(r"^ +Merge branch(es)? '.*$")
-PIntMerge2 = re.compile(r"^ +Merge .*$")
-#
-# Another way to get the statistics (per file).  It implies --numstat
-Pnumstat = re.compile('^(\d+|-)\s+(\d+|-)\s+(.*)$')
-Prename = re.compile('(.*)\{(.*) => (.*)\}(.*)')
+_pemail = r'\s+"?([^<"]+)"?\s<([^>]+)>' # just email addr + name
+
+patterns = {
+    'commit': re.compile (r'^commit ([0-9a-f ]+)$'),
+    'author': re.compile (r'^Author:' + _pemail + '$'),
+    'signed-off-by': re.compile (r'Signed-off-by:' + _pemail),
+    'merge': re.compile (r'^Merge:.*$'),
+    'date': re.compile (r'^(Commit)?Date:\s+(.*)$'),
+    # filea, fileb are used only in 'parche mode' (-p)
+    'filea': re.compile (r'^---\s+(.*)$'),
+    'fileb': re.compile (r'^\+\+\+\s+(.*)$'),
+    'reviewed-by': re.compile (r'Reviewed-by:' + _pemail),
+    'tested-by': re.compile (r' tested-by:' + _pemail, re.I),
+    'reported-by': re.compile (r'Reported-by:' + _pemail),
+    'reported-and-tested-by': re.compile (r'reported-and-tested-by:' + _pemail, re.I),
+    #
+    # Merges are described with a variety of lines.
+    #
+    'ExtMerge': re.compile(r'^ +Merge( branch .* of)? ([^ ]+:[^ ]+)\n$'),
+    'IntMerge': re.compile(r'^ +(Merge|Pull) .* into .*$'),
+    # PIntMerge2 = re.compile(r"^ +Merge branch(es)? '.*$"),
+    'IntMerge2': re.compile(r"^ +Merge .*$"),
+    # Another way to get the statistics (per file).
+    # It implies --numstat
+    'numstat': re.compile('^(\d+|-)\s+(\d+|-)\s+(.*)$'),
+    'rename' : re.compile('(.*)\{(.*) => (.*)\}(.*)'),
+}
